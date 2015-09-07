6ce0ad7b32d153f5e949c86041e7b8ed9abb1a62
Kai Ninomiya <kainino1@gmail.com>
2015-09-07 01:40:43 -0400

    Skeleton w/ stream_compaction as library

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file      main.cpp
+ * @brief     Stream compaction test program
+ * @authors   Kai Ninomiya
+ * @date      2015
+ * @copyright University of Pennsylvania
+ */
+
+int main(int argc, char* argv[]) {
+}
---
+++ stream_compaction/stream_compaction.cu
@@ -0,0 +1,3 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "stream_compaction.h"
