b9ffcb5fd4fdc0b28425216c8347595045d8dfa5
immiao <miaokaixiang@gmail.com>
2017-09-09 03:42:32 -0400

    Tweak

--- src/main.cpp
+++ src/main.cpp
@@ -120,4 +120,6 @@ int main(int argc, char* argv[]) {
     count = StreamCompaction::Efficient::compact(NPOT, c, a);
     //printArray(count, c, true);
     printCmpLenResult(count, expectedNPOT, b, c);
+
+	system("pause");
 }
