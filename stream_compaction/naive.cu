9569f155445ba45f7ee0cd5cb74b5be073e66497
Haoquan Liang <haoquan@seas.upenn.edu>
2022-09-13 16:01:44 -0400

    naive scan done, but there is a better way for buffer swapping -- actually swapping them instead of doing a memcpy

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "naive.h"
 
+#define blockSize 128
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,14 +14,54 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
+        __global__ void KernNaiveScanIteration(int n, int d, int* odata, int* idata)
+        {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n )
+            {
+                return;
+            }
+            if (index >= pow(2, d - 1))
+                odata[index] = idata[index - (int)pow(2, d - 1)] + idata[index];
+            else
+                odata[index] = idata[index];
+        }
+
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
             // TODO
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+            int* dev_odata1;
+            int* dev_odata2;
+            cudaMalloc((void**)&dev_odata1, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_odata1 failed!");
+            cudaMalloc((void**)&dev_odata2, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_odata1 failed!");
+
+            cudaMemcpy(dev_odata1, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+            timer().startGpuTimer();
+            for (int d = 1; d <= ilog2ceil(n); ++d)
+            {
+                KernNaiveScanIteration<< <fullBlocksPerGrid, blockSize >> > (n, d, dev_odata2, dev_odata1);
+                // Buffer swapping
+                cudaMemcpy(dev_odata1, dev_odata2, sizeof(int) * n, cudaMemcpyDeviceToDevice);
+
+            }
+
+            odata[0] = 0;
+            cudaMemcpy(odata + 1, dev_odata1, sizeof(int) * (n - 1), cudaMemcpyDeviceToHost);
             timer().endGpuTimer();
+
+
+            //Clean up
+            cudaFree(dev_odata1);
+            cudaFree(dev_odata2);
+
         }
     }
 }
