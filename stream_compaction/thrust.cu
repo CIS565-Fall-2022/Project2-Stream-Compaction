e55f26885acf1c1f14d50408682332c4eb8ed432
Megan Reddy <meganr28@seas.upenn.edu>
2022-09-15 15:53:32 -0400

    Adding thrust scan

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -22,6 +22,25 @@ namespace StreamCompaction {
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::host_vector<int> host_in(n);
+            thrust::host_vector<int> host_out(n);
+
+            // Copy data into host vectors
+            for (int i = 0; i < n; ++i) {
+                host_in[i] = idata[i];
+                host_out[i] = odata[i];
+            }
+
+            // Create device_vectors from host_vectors
+            thrust::device_vector<int> dev_in(host_in);
+            thrust::device_vector<int> dev_out(host_out);
+
+            thrust::exclusive_scan(dev_in.begin(), dev_in.end(), dev_out.begin());
+
+            // Write final results
+            for (int j = 0; j < n; ++j) {
+                odata[j] = dev_out[j];
+            }
             timer().endGpuTimer();
         }
     }
