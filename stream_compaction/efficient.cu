7aff2590092894527592d97b38664749fa974d34
Tongwei Dai <tongweid@gmail.com>
2022-09-16 15:28:07 -0400

    bank conflict optimization

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -42,16 +42,36 @@ namespace StreamCompaction {
 
         /**
         *  Implementation of scan using shared memory
-        *  reference: GPU Gem3 Listing 39-2
+        *  reference: GPU Gem3 Listing 39-2 to 39-4
         */
 #define SHARED_OPT
-#define SHARED_OPT_BSIZE 2 // how many threads per block in shared memory implementation
+// #define BANK_CONFLICT_OPT
+#define SHARED_OPT_BSIZE 512 // how many threads per block in shared memory implementation
 #define SHARED_OPT_MAX_ARR_SIZE (2 * SHARED_OPT_BSIZE) // maximum array that can be processed by a block in shared memory implementation
+
+
+#ifdef BANK_CONFLICT_OPT
+    #define NUM_BANKS 16
+    #define LOG_NUM_BANKS 4
+    #define CONFLICT_FREE_OFFSET(n) ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))
+#else
+    #define CONFLICT_FREE_OFFSET(n) 0
+#endif // BANK_CONFLICT_OPT
+
         __global__ void kernSharedScan(int N, int* out, int const* in, bool inclusive) {
             __shared__ int temp[SHARED_OPT_MAX_ARR_SIZE];
             int blkid = blockIdx.x;
             int thid = threadIdx.x;
             int offset = 1;
+#ifdef BANK_CONFLICT_OPT
+            int x = thid;
+            int y = thid + (N / 2);
+#else
+            int x = 2 * thid;
+            int y = 2 * thid + 1;
+#endif // BANK_CONFLICT_OPT
+            int bank_offset_A = CONFLICT_FREE_OFFSET(x);
+            int bank_offset_B = CONFLICT_FREE_OFFSET(y);
 
             // pad input and output so we r/w to the correct chunk
             out += blkid * SHARED_OPT_MAX_ARR_SIZE;
@@ -59,14 +79,19 @@ namespace StreamCompaction {
 
             // up sweep
             int saves[2];
-            saves[0] = temp[2 * thid] = in[2 * thid];
-            saves[1] = temp[2 * thid + 1] = in[2 * thid + 1];
+            saves[0] = temp[x + bank_offset_A] = in[x];
+            saves[1] = temp[y + bank_offset_B] = in[y];
 
             for (int d = N >> 1; d > 0; d >>= 1) {
                 __syncthreads();
                 if (thid < d) {
                     int ai = offset * (2 * thid + 1) - 1;
                     int bi = offset * (2 * thid + 2) - 1;
+
+#ifdef BANK_CONFLICT_OPT
+                    ai += CONFLICT_FREE_OFFSET(ai);
+                    bi += CONFLICT_FREE_OFFSET(bi);
+#endif
                     temp[bi] += temp[ai];
                 }
                 offset <<= 1;
@@ -74,7 +99,7 @@ namespace StreamCompaction {
 
             // downsweep
             if (!thid) {
-                temp[N - 1] = 0;
+                temp[N - 1 + CONFLICT_FREE_OFFSET(N-1)] = 0;
             }
 
             for (int d = 1; d < N; d <<= 1) {
@@ -83,6 +108,12 @@ namespace StreamCompaction {
                 if (thid < d) {
                     int ai = offset * (2 * thid + 1) - 1;
                     int bi = offset * (2 * thid + 2) - 1;
+
+#ifdef BANK_CONFLICT_OPT
+                    ai += CONFLICT_FREE_OFFSET(ai);
+                    bi += CONFLICT_FREE_OFFSET(bi);
+#endif
+
                     int save = temp[ai];
                     temp[ai] = temp[bi];
                     temp[bi] += save;
@@ -90,11 +121,11 @@ namespace StreamCompaction {
             }
             __syncthreads();
 
-            out[2 * thid] = temp[2 * thid];
-            out[2 * thid + 1] = temp[2 * thid + 1];
+            out[x] = temp[x + bank_offset_A];
+            out[y] = temp[y + bank_offset_B];
             if (inclusive) {
-                out[2 * thid] += saves[0];
-                out[2 * thid + 1] += saves[1];
+                out[x] += saves[0];
+                out[y] += saves[1];
             }
         }
 
@@ -140,7 +171,6 @@ namespace StreamCompaction {
 #ifdef SHARED_OPT
             if (n <= SHARED_OPT_MAX_ARR_SIZE) {
                 kernSharedScan KERN_PARAM(1, SHARED_OPT_BSIZE) (n, dev_in_out, dev_in_out, inclusive);
-                cudaDeviceSynchronize();
             } else {
                 assert(n % SHARED_OPT_MAX_ARR_SIZE == 0);
 
@@ -149,7 +179,6 @@ namespace StreamCompaction {
 
                 // note: use SHARED_OPT_BSIZE because each thread is responsible for 2 values in the array
                 kernSharedScan KERN_PARAM(nblocks, SHARED_OPT_BSIZE) (SHARED_OPT_MAX_ARR_SIZE, dev_in_out, dev_in_out, true);
-                cudaDeviceSynchronize();
 
                 int* stride;
                 ALLOC(stride, nblocks);
@@ -158,7 +187,6 @@ namespace StreamCompaction {
                 // which has a size of N / SHARED_OPT_MAX_ARR_SIZE
                 int nblocks2 = (nblocks + SHARED_OPT_BSIZE - 1) / SHARED_OPT_BSIZE;
                 kernComputeStride KERN_PARAM(nblocks2, SHARED_OPT_BSIZE) (nblocks, dev_in_out, stride);
-                cudaDeviceSynchronize();
 
                 // PRINT_GPU(dev_in_out, n);
                 scan_impl(nblocks, stride, false);
@@ -166,7 +194,6 @@ namespace StreamCompaction {
 
                 kernComputeFinalArray KERN_PARAM(nblocks, SHARED_OPT_MAX_ARR_SIZE) (n, stride, dev_in_out);
                 // PRINT_GPU(dev_in_out, n);
-                cudaDeviceSynchronize();
 
                 if (!inclusive) {
                     // inclusive to exclusive
