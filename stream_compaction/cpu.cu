a8cfb893a2f309c2362075abf3a9811270d15363
Constance Wang <constancezwang@gmail.com>
2022-09-16 12:55:42 -0400

    add cpu implementations

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -12,6 +12,17 @@ namespace StreamCompaction {
             return timer;
         }
 
+        void exclusiveScan(int n, int* odata, const int* idata) {
+          if (n <= 0) {
+            printf("Empty array!");
+            return;
+          }
+          odata[0] = 0; // identity
+          for (int i = 0; i < n - 1; ++i) {
+            odata[i + 1] = odata[i] + idata[i];
+          }
+        }
+
         /**
          * CPU scan (prefix sum).
          * For performance analysis, this is supposed to be a simple for loop.
@@ -20,6 +31,7 @@ namespace StreamCompaction {
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            exclusiveScan(n, odata, idata);
             timer().endCpuTimer();
         }
 
@@ -31,8 +43,15 @@ namespace StreamCompaction {
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            int outputIndex = 0;
+            for (int inputIndex = 0; inputIndex < n; ++inputIndex) {
+              if (idata[inputIndex] != 0) {
+                odata[outputIndex] = idata[inputIndex];
+                outputIndex += 1;
+              }
+            }
             timer().endCpuTimer();
-            return -1;
+            return outputIndex;
         }
 
         /**
@@ -43,8 +62,40 @@ namespace StreamCompaction {
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            // calculate bool. array
+            int* boolData = new int[n];
+            if (boolData == NULL) {
+              printf("Failed to allocate boolData");
+              return -1;
+            }
+            for (int i = 0; i < n; ++i) {
+              boolData[i] = (idata[i] == 0) ? 0 : 1;
+            }
+
+            int* boolScan = new int[n];
+            if (boolScan == NULL) {
+              printf("Failed to allocate boolScan");
+              return -1;
+            }
+            exclusiveScan(n, boolScan, boolData);
+            //printf("boolScan ");
+            //for (int i = 0; i < n; ++i) {
+            //  printf("%d ", boolScan[i]);
+            //}
+
+            int outIndex = -1;
+            // Scatter
+            for (int i = 0; i < n; ++i) {
+              if (boolData[i] == 1) {
+                outIndex = boolScan[i];
+                odata[outIndex] = idata[i];
+              }
+            }
+            delete[] boolData;
+            delete[] boolScan;
             timer().endCpuTimer();
-            return -1;
+
+            return outIndex + 1;
         }
     }
 }
