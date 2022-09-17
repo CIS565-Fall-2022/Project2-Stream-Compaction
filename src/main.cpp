7fecfebbefb439365f7692b511d968a312ccf9a4
IwakuraRein <28486541+IwakuraRein@users.noreply.github.com>
2022-09-17 18:22:52 -0400

    work-efficient scan, power-of-two

--- src/main.cpp
+++ src/main.cpp
@@ -71,38 +71,28 @@ int main(int argc, char* argv[]) {
     printDesc("work-efficient scan, power-of-two");
     StreamCompaction::Efficient::scan(SIZE, c, a);
     printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //int tmp[64] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64 };
-    //StreamCompaction::Efficient::scan(64, c, tmp);
 
-    for (int i = 0; i < 512; i++) {
-        std::cout << a[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << std::endl;
-    
-    for (int i = 0; i < 512; i++) {
-        std::cout << c[i] << " ";
-    }
-    std::cout << std::endl;   
-    std::cout << std::endl;   
-    //for (int i = 511-64; i < 512; i++) {
-    //    std::cout << c[i] << " ";
+    //for (int i = 0; i < 512; i++) {
+    //    std::cout << a[i] << " ";
     //}
     //std::cout << std::endl;
-    //StreamCompaction::CPU::scan(64, c, tmp);
-    //for (int i = 0; i < 64; i++) {
+    //std::cout << std::endl;
+    //
+    //for (int i = 0; i < 512; i++) {
     //    std::cout << c[i] << " ";
     //}
+    //std::cout << std::endl;   
     //std::cout << std::endl;
+    // 
     //printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
-    //zeroArray(SIZE, c);
-    //printDesc("work-efficient scan, non-power-of-two");
-    //StreamCompaction::Efficient::scan(NPOT, c, a);
-    //printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    ////printArray(NPOT, c, true);
-    //printCmpResult(NPOT, b, c);
+    zeroArray(SIZE, c);
+    printDesc("work-efficient scan, non-power-of-two");
+    StreamCompaction::Efficient::scan(NPOT, c, a);
+    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
+    //printArray(NPOT, c, true);
+    printCmpResult(NPOT, b, c);
 
     zeroArray(SIZE, c);
     printDesc("thrust scan, power-of-two");
--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -17,37 +17,20 @@ namespace StreamCompaction {
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
-        __global__ void kernUpSweep(int n, int log2n, int* inp) {
+        __global__ void kernUpSweep(int n, int num, int offset, int* inp) {
             int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-            if (idx >= n) return;
-            int num = n / 2;
-            for (int d = 0; d < log2n; d++) {
-                __syncthreads();
-                if (idx >= num) continue;
-                int offset = 1 << d; // 1 << d is pow(2, d)
-                int idxWrite = offset * 2 * (idx + 1) - 1;
-                inp[idxWrite] = inp[idxWrite] + inp[idxWrite - offset];
-                num /= 2;
-            }
-            //__syncthreads();
-            //if (idx == n - 1) inp[n - 1] = 0;
-            //¡ü doesn't work when n is large ¡ü
+            if (idx >= num) return;
+            int idxWrite = offset * 2 * (idx + 1) - 1;
+            inp[idxWrite] = inp[idxWrite] + inp[idxWrite - offset];
         }
 
-        __global__ void kernDownSweep(int n, int log2n, int* inp) {
+        __global__ void kernDownSweep(int n, int num, int offset, int* inp) {
             int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-            if (idx >= n) return;
-            int offset = n / 2;
-            for (int d = 0; d < log2n; d++) {
-                __syncthreads();
-                if (idx < 1 << d) {
-                    int idxWrite = n - 1 - idx * offset * 2;
-                    int tmp = inp[idxWrite];
-                    inp[idxWrite] += inp[idxWrite - offset];
-                    inp[idxWrite - offset] = tmp;
-                }
-                offset /= 2;
-            }
+            if (idx >= num) return;
+            int idxWrite = n - 1 - idx * offset * 2;
+            int tmp = inp[idxWrite];
+            inp[idxWrite] += inp[idxWrite - offset];
+            inp[idxWrite - offset] = tmp;
         }
 
         void scan(int n, int *odata, const int *idata, bool enableTimer) {
@@ -58,10 +41,26 @@ namespace StreamCompaction {
             checkCUDAError("cudaMemcpy idata failed!");
 
             if (enableTimer) timer().startGpuTimer();
-            dim3 fullBlocksPerGrid((n + BLOCK_SIZE - 1) / BLOCK_SIZE);
-            kernUpSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, ilog2ceil(n), devInp);
+
+            // up sweep
+            int num = n / 2;
+            for (int d = 0; d < ilog2ceil(n); d++) {
+                int offset = 1 << d;
+                dim3 fullBlocksPerGrid((num + BLOCK_SIZE - 1) / BLOCK_SIZE);
+                kernUpSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, num, offset, devInp);
+                num /= 2;
+            }
             cudaMemset(devInp+n-1, 0, sizeof(int));
-            kernDownSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, ilog2ceil(n), devInp);
+
+            // down sweep
+            int offset = n / 2;
+            for (int d = 0; d < ilog2ceil(n); d++) {
+                int num = 1 << d;
+                dim3 fullBlocksPerGrid((num + BLOCK_SIZE - 1) / BLOCK_SIZE);
+                kernDownSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>> (n, num, offset, devInp);
+                offset /= 2;
+            }
+
             if (enableTimer) timer().endGpuTimer();
 
             cudaMemcpy(odata, devInp, n * sizeof(int), cudaMemcpyDeviceToHost);
@@ -82,43 +81,60 @@ namespace StreamCompaction {
 
 
         int compact(int n, int *odata, const int *idata, bool enableTimer) {
-            //int* devInp;
-            //int* devBools;
-            //int* devOut;
-            //cudaMalloc((void**)&devInp, n * sizeof(int));
-            //checkCUDAError("cudaMalloc devInp failed!");
-            //cudaMalloc((void**)&devOut, n * sizeof(int));
-            //checkCUDAError("cudaMalloc devOut failed!");
-            //cudaMalloc((void**)&devBools, n * sizeof(int));
-            //checkCUDAError("cudaMalloc devBools failed!");
-            //cudaMemcpy(devInp, idata, n * sizeof(int), cudaMemcpyHostToDevice);
-            //checkCUDAError("cudaMemcpy idata failed!");
+            int* devInp;
+            int* devBools;
+            int* devOut;
+            cudaMalloc((void**)&devInp, n * sizeof(int));
+            checkCUDAError("cudaMalloc devInp failed!");
+            cudaMalloc((void**)&devOut, n * sizeof(int));
+            checkCUDAError("cudaMalloc devOut failed!");
+            cudaMalloc((void**)&devBools, n * sizeof(int));
+            checkCUDAError("cudaMalloc devBools failed!");
+            cudaMemcpy(devInp, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy idata failed!");
 
-            //if (enableTimer) timer().startGpuTimer();
-            //dim3 fullBlocksPerGrid((n + BLOCK_SIZE - 1) / BLOCK_SIZE);
-            //Common::kernMapToBoolean<<<fullBlocksPerGrid, BLOCK_SIZE>>> (n, devBools, devInp);
-            //kernUpSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, ilog2ceil(n), devBools);
-            //cudaMemset(devBools +n-1, 0, sizeof(int));
-            //kernDownSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, ilog2ceil(n), devBools);
-            //Common::kernScatter<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, devOut, devInp, devBools);
-            //if (enableTimer) timer().endGpuTimer();
+            if (enableTimer) timer().startGpuTimer();
+            dim3 fullBlocksPerGrid((n + BLOCK_SIZE - 1) / BLOCK_SIZE);
+            Common::kernMapToBoolean<<<fullBlocksPerGrid, BLOCK_SIZE>>> (n, devBools, devInp);
 
-            //cudaMemcpy(odata, devOut, n * sizeof(int), cudaMemcpyDeviceToHost);
-            //checkCUDAError("cudaMemcpy odata failed!");
-            //std::unique_ptr<int[]> indices{ new int[n] };
-            //cudaMemcpy(indices.get(), devBools, n * sizeof(int), cudaMemcpyDeviceToHost);
-            //checkCUDAError("cudaMemcpy devBools failed!");
-            //cudaFree(devInp);
-            //checkCUDAError("cudaFree devInp failed!");
-            //cudaFree(devOut);
-            //checkCUDAError("cudaFree devInp failed!");
-            //cudaFree(devBools);
-            //checkCUDAError("cudaFree devBools failed!");
-            ////for (int i = 0; i < 32; i++) {
-            ////    std::cout << indices[i] << " ";
-            ////}
-            ////std::cout << std::endl;
-            //return idata[n - 1] != 0 ? indices[n - 1] + 1 : indices[n - 1];
+            // up sweep
+            int num = n / 2;
+            for (int d = 0; d < ilog2ceil(n); d++) {
+                int offset = 1 << d;
+                dim3 fullBlocksPerGrid((num + BLOCK_SIZE - 1) / BLOCK_SIZE);
+                kernUpSweep << <fullBlocksPerGrid, BLOCK_SIZE >> > (n, num, offset, devBools);
+                num /= 2;
+            }
+            cudaMemset(devBools + n - 1, 0, sizeof(int));
+
+            // down sweep
+            int offset = n / 2;
+            for (int d = 0; d < ilog2ceil(n); d++) {
+                int num = 1 << d;
+                dim3 fullBlocksPerGrid((num + BLOCK_SIZE - 1) / BLOCK_SIZE);
+                kernDownSweep << <fullBlocksPerGrid, BLOCK_SIZE >> > (n, num, offset, devBools);
+                offset /= 2;
+            }
+
+            Common::kernScatter<<<fullBlocksPerGrid, BLOCK_SIZE>>>(n, devOut, devInp, devBools);
+            if (enableTimer) timer().endGpuTimer();
+
+            cudaMemcpy(odata, devOut, n * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy odata failed!");
+            std::unique_ptr<int[]> indices{ new int[n] };
+            cudaMemcpy(indices.get(), devBools, n * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy devBools failed!");
+            cudaFree(devInp);
+            checkCUDAError("cudaFree devInp failed!");
+            cudaFree(devOut);
+            checkCUDAError("cudaFree devInp failed!");
+            cudaFree(devBools);
+            checkCUDAError("cudaFree devBools failed!");
+            //for (int i = 0; i < 32; i++) {
+            //    std::cout << indices[i] << " ";
+            //}
+            //std::cout << std::endl;
+            return idata[n - 1] != 0 ? indices[n - 1] + 1 : indices[n - 1];
             return -1;
         }
     }
