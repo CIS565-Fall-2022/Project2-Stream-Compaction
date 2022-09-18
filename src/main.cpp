97594910f31cab089c39c82a3981fd8ca340e3c9
esiaero <anthias@seas.upenn.edu>
2022-09-18 14:05:28 -0400

    efficient scan

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <stream_compaction/thrust.h>
 #include "testing_helpers.hpp"
 
-const int SIZE = 1 << 8; // feel free to change the size of array
+const int SIZE = 1 << 18; // feel free to change the size of array
 const int NPOT = SIZE - 3; // Non-Power-Of-Two
 int *a = new int[SIZE];
 int *b = new int[SIZE];
@@ -54,31 +54,31 @@ int main(int argc, char* argv[]) {
     printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
-    /* For bug-finding only: Array of 1s to help find bugs in stream compaction or scan
-    onesArray(SIZE, c);
-    printDesc("1s array for finding bugs");
-    StreamCompaction::Naive::scan(SIZE, c, a);
-    printArray(SIZE, c, true); */
-
     zeroArray(SIZE, c);
     printDesc("naive scan, non-power-of-two");
     StreamCompaction::Naive::scan(NPOT, c, a);
     printElapsedTime(StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    printArray(SIZE, c, true);
+    printArray(NPOT, c, true);
     printCmpResult(NPOT, b, c);
+
+    // For bug-finding only: Array of 1s to help find bugs in stream compaction or scan
+    //onesArray(NPOT, c);
+    //printDesc("1s array for finding bugs, NPOT");
+    //StreamCompaction::Efficient::scan(NPOT, a, c);
+    //printArray(NPOT, a, true);
 
     zeroArray(SIZE, c);
     printDesc("work-efficient scan, power-of-two");
     StreamCompaction::Efficient::scan(SIZE, c, a);
     printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
     zeroArray(SIZE, c);
     printDesc("work-efficient scan, non-power-of-two");
     StreamCompaction::Efficient::scan(NPOT, c, a);
     printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(NPOT, c, true);
+    printArray(NPOT, c, true);
     printCmpResult(NPOT, b, c);
 
     zeroArray(SIZE, c);
--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -2,6 +2,9 @@
 #include <cuda_runtime.h>
 #include "common.h"
 #include "efficient.h"
+#include <device_launch_parameters.h>
+#include <iostream>
+
 
 namespace StreamCompaction {
     namespace Efficient {
@@ -11,13 +14,63 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
+        
+        constexpr unsigned blockSize = 128; // TODO test different blockSizes
+
+        __global__ void kernUpsweep(int d, int n, int* data) {
+            unsigned index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            unsigned rightPOT = 1 << (d + 1);
+            index *= rightPOT; // "by 2^(d+1)"
+            unsigned rightIdx = index + rightPOT - 1;
+            if (rightIdx > n) { return; }
+            data[rightIdx] += data[index + (1 << d) - 1];
+        }
+
+        __global__ void kernDownsweep(int d, int n, int* data) {
+            unsigned index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            unsigned rightPOT = 1 << (d + 1);
+            index *= rightPOT;
+            unsigned leftIdx = index + (1 << d) - 1;
+            unsigned rightIdx = index + rightPOT - 1;
+            if (rightIdx > n) { return; }
+
+            int tmp = data[leftIdx];
+            data[leftIdx] = data[rightIdx];
+            data[rightIdx] += tmp;
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+            int smallestPOTGreater = 1 << ilog2ceil(n); // smallest POT larger than n
+
+            int* dev_data;
+            cudaMalloc((void**)&dev_data, smallestPOTGreater * sizeof(int));
+            cudaMemcpy(dev_data, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            cudaMemset(dev_data + n, 0, (smallestPOTGreater - n) * sizeof(int)); // necessary? 
+
+            int neededThreads = smallestPOTGreater;
+
+            dim3 fullBlocksPerGrid;
+            for (int d = 0; d < ilog2ceil(smallestPOTGreater); ++d, neededThreads /= 2) {
+                fullBlocksPerGrid = (neededThreads + blockSize - 1) / blockSize;
+                kernUpsweep<<<fullBlocksPerGrid, blockSize>>>(d, n, dev_data);
+                cudaDeviceSynchronize();
+            }
+
+            cudaMemset(&dev_data[smallestPOTGreater - 1], 0, sizeof(int));
+
+            for (int d = ilog2ceil(smallestPOTGreater) - 1; d >= 0; --d, neededThreads *= 2) {
+                fullBlocksPerGrid = (neededThreads + blockSize - 1) / blockSize;
+                kernDownsweep<<<fullBlocksPerGrid, blockSize>>>(d, smallestPOTGreater, dev_data);
+                cudaDeviceSynchronize();
+            }
+
+            cudaMemcpy(odata, dev_data, n * sizeof(int), cudaMemcpyDeviceToHost);
+            cudaFree(dev_data);
             timer().endGpuTimer();
         }
 
