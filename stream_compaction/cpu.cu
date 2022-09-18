5af4492467e89abc781d06c340c22363503ce1e9
Shineng Tang <45440408+jaAcKrABbit@users.noreply.github.com>
2022-09-18 12:04:29 -0400

    finished optimizing block counts

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -20,6 +20,10 @@ namespace StreamCompaction {
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            odata[0] = 0;
+            for (int i = 1; i < n; i++) {
+              odata[i] = odata[i - 1] + idata[i - 1];
+            }
             timer().endCpuTimer();
         }
 
@@ -31,8 +35,14 @@ namespace StreamCompaction {
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+              if (idata[i] != 0) {
+                odata[count++] = idata[i];
+              }
+            }
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
 
         /**
@@ -43,8 +53,26 @@ namespace StreamCompaction {
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
             // TODO
+            int* temp = new int[n];
+            for (int i = 0; i < n; i++) {
+              temp[i] = idata[i] == 0 ? 0 : 1;
+            }
+            //scan result
+            odata[0] = 0;
+            for (int i = 1; i < n; i++) {
+              odata[i] = odata[i - 1] + temp[i - 1];
+            }
+            //int count = odata[n - 1];
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+              if (idata[i] != 0) {
+                odata[odata[i]] = idata[i];
+                count++;
+              }
+            }
             timer().endCpuTimer();
-            return -1;
+            delete temp;
+            return count;
         }
     }
 }
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,10 @@
 #include "common.h"
 #include "naive.h"
 
+
+
+#define blockSize 128
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,14 +16,49 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
-
+        __global__ void kernScan(int n, int d, int* odata, const int* idata) {
+            int k = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (k >= n) {
+                return;
+            }
+            int stride = 1 << (d - 1);
+            if (k >= stride) {
+                odata[k] = idata[k - stride] + idata[k];
+            }
+            else {
+                odata[k] = idata[k];
+            }
+        }
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
-         */
+         */     
         void scan(int n, int *odata, const int *idata) {
+            int* dev_idata;
+            int* dev_odata;
+
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+
+            cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+          //  cudaMemcpy(dev_odata, odata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
             timer().startGpuTimer();
             // TODO
+            for (int d = 1; d <= ilog2ceil(n); d++) {
+                kernScan << <fullBlocksPerGrid, blockSize >> > (n, d, dev_odata, dev_idata);
+                std::swap(dev_odata, dev_idata);
+                cudaMemcpy(odata , dev_idata, sizeof(int) * (n), cudaMemcpyDeviceToHost);
+            }
             timer().endGpuTimer();
+            //convert from inclusive scan to exclusive scan
+            odata[0] = 0;
+            cudaMemcpy(odata + 1, dev_idata, sizeof(int) * (n - 1), cudaMemcpyDeviceToHost);
+
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
+            
         }
     }
 }
