99fa0f778e7ca91c487babd4b8e87061ef046097
THOMAS DONNELLY <tomvdon@park.key.seas.upenn.edu>
2022-09-19 12:12:47 -0400

    some Opt

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -12,6 +12,36 @@ namespace StreamCompaction {
             return timer;
         }
 
+        __global__ void kernUpSweepOpt(int n, int depth, int offset, int* data)
+        {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            int offset_1 = offset << 1;
+            if (index > n)
+            {
+                return;
+            }
+            data[index * offset_1 + offset_1 - 1] += data[index * offset_1 + offset - 1];
+
+        }
+        __global__ void kernDownSweepOpt(int n, int depth, int offset, int* data, bool root)
+        {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            int offset_1 = offset << 1;
+            if (index > n)
+            {
+                return;
+            }
+            if (index == (n - 1) && root)
+            {
+                data[index] = 0;
+            }
+            int t = data[index * offset_1 + offset -1];
+            data[index * offset_1 + offset - 1] = data[index*offset_1 + offset_1 -1];
+            data[index * offset_1 + offset_1 -1 ] += t;
+          
+
+        }
+
         __global__ void kernUpSweep(int n, int depth, int offset, int* data)
         {
             int index = threadIdx.x + (blockIdx.x * blockDim.x);
@@ -48,7 +78,7 @@ namespace StreamCompaction {
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
-        void scan(int n, int *odata, const int *idata) {
+        void scanNoOpt(int n, int *odata, const int *idata) {
             int blockSize = 128;
             int numBlocks = ((n + blockSize - 1) / blockSize);
             int* dev_data;
@@ -72,6 +102,39 @@ namespace StreamCompaction {
                 int offset = 1 << depth;
                 kernDownSweep << < numBlocks, blockSize >> > (pot, depth, offset, dev_data, root);
                 root = false;
+            }
+            timer().endGpuTimer();
+            cudaMemcpy(odata, dev_data, sizeof(int) * n, cudaMemcpyDeviceToHost);
+            cudaFree(dev_data);
+        }
+
+        void scan(int n, int* odata, const int* idata) {
+            int blockSize = 128;
+            int numBlocks = ((n + blockSize - 1) / blockSize);
+            int* dev_data;
+            float d = ilog2ceil(n);
+            int pot = pow(2, d);
+            cudaMalloc((void**)&dev_data, pot * sizeof(int));
+            cudaMemset(dev_data, 0, pot * sizeof(int));
+            cudaMemcpy(dev_data, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            cudaDeviceSynchronize();
+            int num = pot;
+            timer().startGpuTimer();
+            for (int depth = 0; depth < d; depth++)
+            {
+                int offset = 1 << depth;
+                kernUpSweep << < numBlocks, blockSize >> > (num, depth, offset, dev_data);
+                num /= 2;
+
+            }
+            bool root = true;
+            cudaDeviceSynchronize();
+            for (int depth = d - 1; depth > 0; depth--)
+            {
+                int offset = 1 << depth;
+                kernDownSweep << < numBlocks, blockSize >> > (num, depth, offset, dev_data, root);
+                root = false;
+                num *= 2;
             }
             timer().endGpuTimer();
             cudaMemcpy(odata, dev_data, sizeof(int) * n, cudaMemcpyDeviceToHost);
