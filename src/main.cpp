ea9e243ecb99b3e073df54843afbf0b1f38e1391
tomvdon <tomvdon@gmail.com>
2022-09-19 23:19:56 -0400

    README Update

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,13 @@
 #include <stream_compaction/thrust.h>
 #include "testing_helpers.hpp"
 
-const int SIZE = 1 << 20; // feel free to change the size of array
+const int SIZE = 1 << 8; // feel free to change the size of array
 const int NPOT = SIZE - 3; // Non-Power-Of-Two
 int *a = new int[SIZE];
 int *b = new int[SIZE];
 int *c = new int[SIZE];
 
 int main(int argc, char* argv[]) {
-    //Preliminary tests
-    printf("\n");
-    printf("****************\n");
-    printf("** PRE TESTS! **\n");
-    printf("****************\n");
-    checkCUDAError("FUN");
     // Scan tests
 
     printf("\n");
